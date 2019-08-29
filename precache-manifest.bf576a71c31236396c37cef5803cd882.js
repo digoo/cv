67407d25f8426837c751fa5b1f96f030040605fe
Rodrigo Lopes <rocostalopes@gmail.com>
2019-08-29 01:51:51 -0300

    Updates

---
+++ precache-manifest.bf576a71c31236396c37cef5803cd882.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1671d6f225d1f080d0dd887646776ca8",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "fccbfb4aa07091cfa8b5",
+    "url": "/cv/static/js/2.316fd937.chunk.js"
+  },
+  {
+    "revision": "53116a95048d438a3d95",
+    "url": "/cv/static/js/main.752ab43f.chunk.js"
+  },
+  {
+    "revision": "9de11688f3f3e719fc9b",
+    "url": "/cv/static/js/runtime~main.2485d320.js"
+  },
+  {
+    "revision": "ded805e44ca0aeffb0cc60cd107ef77d",
+    "url": "/cv/static/media/Banner.ded805e4.jpg"
+  },
+  {
+    "revision": "0f276b9e3498adec37a8ab176b9c2848",
+    "url": "/cv/static/media/aRodrigo.0f276b9e.jpg"
+  },
+  {
+    "revision": "43a084eecf9e17a9486216e8bbd8bd27",
+    "url": "/cv/static/media/addfriend.43a084ee.svg"
+  },
+  {
+    "revision": "2288ae6ce6fc1742d58f11fbdc406543",
+    "url": "/cv/static/media/avatar.2288ae6c.png"
+  },
+  {
+    "revision": "29360e7857aa7c5c358c7c5a545a373a",
+    "url": "/cv/static/media/avatarDigo.29360e78.jpg"
+  },
+  {
+    "revision": "e2e14f08707719d609ed618e1a5edd82",
+    "url": "/cv/static/media/born.e2e14f08.svg"
+  },
+  {
+    "revision": "3adaceb79afb3be809c4c924179bf296",
+    "url": "/cv/static/media/comments.3adaceb7.svg"
+  },
+  {
+    "revision": "4c98b3ff22f3d35cb7b8371ce8595fa2",
+    "url": "/cv/static/media/comunikimeT.4c98b3ff.png"
+  },
+  {
+    "revision": "5126fd3bbb07a327df67ee4b2805ca5d",
+    "url": "/cv/static/media/followers.5126fd3b.svg"
+  },
+  {
+    "revision": "d4f69d9cdcda013dc34549636a67faaa",
+    "url": "/cv/static/media/github.d4f69d9c.svg"
+  },
+  {
+    "revision": "869444c3464303f9d80b4cd0eff50c79",
+    "url": "/cv/static/media/home.869444c3.svg"
+  },
+  {
+    "revision": "b10527e9e3a6b87b493262aa9fd9e456",
+    "url": "/cv/static/media/images.b10527e9.svg"
+  },
+  {
+    "revision": "fffd4ba33e4ee1467c9739a37f34daff",
+    "url": "/cv/static/media/like.fffd4ba3.svg"
+  },
+  {
+    "revision": "4547aad5f47b7e15b0ed6e88e1076138",
+    "url": "/cv/static/media/linkedin.4547aad5.svg"
+  },
+  {
+    "revision": "fa3b4938f7a391d205ada8c12e3069fb",
+    "url": "/cv/static/media/logo.fa3b4938.svg"
+  },
+  {
+    "revision": "9a0b7da06f7ed3b8069f5dd880bca100",
+    "url": "/cv/static/media/message.9a0b7da0.svg"
+  },
+  {
+    "revision": "05409250d2a79376f9f57287cc3fb249",
+    "url": "/cv/static/media/message2.05409250.svg"
+  },
+  {
+    "revision": "f78025807fe5c9fa4a3771dece37454f",
+    "url": "/cv/static/media/more.f7802580.svg"
+  },
+  {
+    "revision": "70fecc949ce923fade46513686c67bf5",
+    "url": "/cv/static/media/notification.70fecc94.svg"
+  },
+  {
+    "revision": "9cf18e7defb3c9018866fdd125270537",
+    "url": "/cv/static/media/place.9cf18e7d.svg"
+  },
+  {
+    "revision": "3d67b80e6004efc04b0f8487a578b333",
+    "url": "/cv/static/media/reactjs.3d67b80e.png"
+  },
+  {
+    "revision": "978cc68bd4955ee39bc5ab92e592d7b5",
+    "url": "/cv/static/media/retweet.978cc68b.svg"
+  },
+  {
+    "revision": "6f5bf5745fb51d157c271bdec4e1c486",
+    "url": "/cv/static/media/search.6f5bf574.svg"
+  },
+  {
+    "revision": "f98d31800f994d67c994d5752520c7d6",
+    "url": "/cv/static/media/whatsapp.f98d3180.svg"
+  }
+]);
