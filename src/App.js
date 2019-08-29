b8717637565ff8eeca6a4c141a03b5af336d00ad
digoo <digoo@hotmail.de>
2019-08-29 19:42:29 -0300

    changed to use MemoryRouter page is not changing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// import { Router } from 'react-router-dom';
 
+// import history from './services/history';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
@@ -229,7 +231,9 @@ export default function App() {
             </div>
           </aside>
 
+          {/* <Router history={history}> */}
           <Routes />
+          {/* </Router> */}
 
           <aside className="widgets">
             <div className="widget follow">
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
 import Exp from './pages/Exp';
 import Edu from './pages/Edu';
@@ -7,12 +7,13 @@ import Port from './pages/Port';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/cv/" exact component={Exp} />
-        <Route path="/cv/education" component={Edu} />
-        <Route path="/cv/portfolio" component={Port} />
-      </Switch>
-    </BrowserRouter>
+    <MemoryRouter>
+      {/* <Switch> */}
+      <Route exact path="/" component={Exp} />
+      <Route exact path="/cv/" component={Exp} />
+      <Route path="/cv/education" component={Edu} />
+      <Route path="/cv/portfolio" component={Port} />
+      {/* </Switch> */}
+    </MemoryRouter>
   );
 }
